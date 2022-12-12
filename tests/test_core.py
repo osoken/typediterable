380cdafb0408218f83f748457fed4689e9aad994
osoken <osoken@osoekawalab.com>
2022-12-12 12:54:22 +0900

    test(core): add test for default error handling

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,16 @@
+import pytest
+
 import typingiterable
 
 
 def test_iterate() -> None:
     actual = list(typingiterable.TypingIterable[int](["122", "231", "0", "2", 2.3]))
     assert actual == [122, 231, 0, 2, 2]
+
+
+def test_default_error_handling() -> None:
+    actual = []
+    with pytest.raises(Exception):
+        for d in typingiterable.TypingIterable[int](["123", "321", "1.23", "432"]):
+            actual.append(d)
+    assert actual == [123, 321]
