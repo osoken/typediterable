c8e3209a1a604397c209fc2a256392b99894fca2
osoken <osoken@osoekawalab.com>
2022-12-14 20:26:10 +0900

    feat(core): add variable length argument

--- typingiterable/core.py
+++ typingiterable/core.py
@@ -1,12 +1,22 @@
 from collections.abc import Callable, Iterable
+from enum import Enum
 from typing import Any, Generic, Optional, Type, TypeVar
 
 T = TypeVar("T")
 
 
+class ArgumentType(str, Enum):
+    one_argument = "one_argument"
+    variable_length_argument = "variable_length_argument"
+    variable_length_keyword_argument = "variable_length_keyword_argument"
+
+
 class GenericTypingIterable(Generic[T]):
     def __init__(self, t: Type[T]):
         self._t = t
+
+    def _cast(self, d: Any) -> T:
+        return self._t(d)
 
     def __call__(
         self, iter: Iterable[Any], on_error: Optional[Callable[[Any, int, Exception], None]] = None
@@ -14,17 +24,38 @@ class GenericTypingIterable(Generic[T]):
         if on_error is not None:
             for i, d in enumerate(iter):
                 try:
-                    yield self._t(d)  # type: ignore [call-arg]
+                    yield self._cast(d)  # type: ignore [call-arg]
                 except Exception as e:
                     on_error(d, i, e)
         else:
             for d in iter:
-                yield self._t(d)  # type: ignore [call-arg]
+                yield self._cast(d)  # type: ignore [call-arg]
+
+
+class GenericVariableLengthArgumentTypingIterable(Generic[T], GenericTypingIterable[T]):
+    def _cast(self, d: Any) -> T:
+        return self._t(*d)
+
+
+class GenericVariableLengthArgumentKeywordTypingIterable(Generic[T], GenericTypingIterable[T]):
+    def _cast(self, d: Any) -> T:
+        return self._t(**d)
 
 
 class GenericTypingIterableFactory:
+    def __init__(self, argument_type: ArgumentType = ArgumentType.one_argument):
+        self._argument_type = argument_type
+
     def __getitem__(self, t: Type[T]) -> GenericTypingIterable[T]:
+        if self._argument_type == ArgumentType.variable_length_argument:
+            return GenericVariableLengthArgumentTypingIterable[T](t)
+        if self._argument_type == ArgumentType.variable_length_keyword_argument:
+            return GenericVariableLengthArgumentKeywordTypingIterable[T](t)
         return GenericTypingIterable[T](t)
 
 
 TypingIterable = GenericTypingIterableFactory()
+VariableLengthArgumentTypingIterable = GenericTypingIterableFactory(argument_type=ArgumentType.variable_length_argument)
+VariableLengthKeywordArgumentTypingIterable = GenericTypingIterableFactory(
+    argument_type=ArgumentType.variable_length_keyword_argument
+)
