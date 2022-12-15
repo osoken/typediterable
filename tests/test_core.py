943bf4d18059eb8606198799e54262a550c11d22
osoken <osoken.devel@outlook.jp>
2022-12-15 21:58:10 +0900

    test(core): add test case for keyword-only argument type

--- tests/test_core.py
+++ tests/test_core.py
@@ -24,6 +24,23 @@ class TwoArgumentDataType:
         return self.x == other.x and self.y == other.y
 
 
+class KeywordOnlyArgumentDataType(TwoArgumentDataType):
+    def __init__(self, *, x: int, y: int, text: str):
+        super(KeywordOnlyArgumentDataType, self).__init__(x, y)
+        self._text = text
+
+    @property
+    def text(self) -> str:
+        return self._text
+
+    def __eq__(self, other: Any) -> bool:
+        if not super(KeywordOnlyArgumentDataType, self).__eq__(other):
+            return False
+        if not isinstance(other, KeywordOnlyArgumentDataType):
+            return False
+        return self.text == other.text
+
+
 def test_iterate() -> None:
     actual = list(typingiterable.TypingIterable[int](["122", "231", "0", "2", 2.3]))
     assert actual == [122, 231, 0, 2, 2]
@@ -70,4 +87,24 @@ def test_variable_length_keyword_arguments() -> None:
         TwoArgumentDataType(-1, 3),
         TwoArgumentDataType(-3, -3),
         TwoArgumentDataType(0, 0),
+    ]
+
+
+def test_keyword_only_arguments() -> None:
+    actual = []
+    for d in typingiterable.VariableLengthKeywordArgumentTypingIterable[KeywordOnlyArgumentDataType](
+        [
+            {"x": 10, "y": 12, "text": "one"},
+            {"x": -1, "y": 3, "text": "two"},
+            {"x": -3, "y": -3, "text": "three"},
+            {"y": 0, "x": 0, "text": "four"},
+        ]
+    ):
+        actual.append(d)
+
+    assert actual == [
+        KeywordOnlyArgumentDataType(x=10, y=12, text="one"),
+        KeywordOnlyArgumentDataType(x=-1, y=3, text="two"),
+        KeywordOnlyArgumentDataType(x=-3, y=-3, text="three"),
+        KeywordOnlyArgumentDataType(x=0, y=0, text="four"),
     ]
